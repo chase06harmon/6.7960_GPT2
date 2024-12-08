b2dd20a233935945f603a26bb05759755e35ad4e
chaseharmon <chase1harmon@gmail.com>
2024-12-08 18:06:45 -0500

    changes run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J classifierTrainer_4GPUs
+#SBATCH -J gpt2_training
 #SBATCH -o myjob_4GPUs_%j.out
 #SBATCH -e myjob_4GPUs_%j.err
 #SBATCH --gres=gpu:4
@@ -10,18 +10,17 @@
 #SBATCH --time=9:33:33
 #SBATCH --exclusive
 
-module load anaconda3/2020.02-2ks5tch
 module load cuda/11.8
 
 #activate environment
-source /home/paulg9/miniforge3/bin/activate
+source ~/miniforge3/envs/gpt2_env/bin/activate
 
-srun /home/paulg9/miniforge3/bin/python run_clm.py \
+srun ~/miniforge3/bin/python run_clm.py \
     --model_type gpt2 \
     --tokenizer_name gpt2 \
-    --dataset_name chaseharmon/6.7960_FINAL \
-    --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 2 \
+    --dataset_name ./shake_gpt2_train.txt \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
     --do_train \
     --do_eval \
     --output_dir /tmp/test-clm \
